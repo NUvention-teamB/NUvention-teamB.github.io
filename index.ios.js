f38159ba99df1c095b91829f4359f26de0dbda40
Christopher Li <Christopherli2017@u.northwestern.edu>
2017-05-16 01:09:11 -0500

    commit import statement

--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { LoginButton, AccessToken } from 'react-native-fbsdk'
 import { AWSCognitoCredentials } from 'aws-sdk-react-native-core'
-import { AWSDynamoDB } from 'aws-sdk-react-native-dynamodb'
+// import { AWSDynamoDB } from 'aws-sdk-react-native-dynamodb'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { RNS3 } from 'react-native-aws3';
 import { getPageID, getPageAccessToken, pagePost } from './lib/FacebookAPI'
